c5ef9d883d522d0d5ed36b64c8f0fd64c6c76054
Gabriel Fernandes <gabrielfsilva09@gmail.com>
2024-02-14 15:21:39 -0300

    splash-screen

---
+++ src/components/atoms/Logo/index.js
@@ -0,0 +1,7 @@
+import { LogoImage } from "./styles";
+import React from "react";
+import logoImage from "../../../../assets/LogoEmpire.png";
+
+export const Logo = () => {
+    return <LogoImage source={logoImage} />;
+};
